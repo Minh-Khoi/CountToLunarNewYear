dd6c845c216c67d5f4a4c827c8cb06e6358d6185
Minh kHoi <nguyenxngocminhkhoi@gmail.com>
2020-01-24 11:20:05 +0700

    fix some bugs

--- src/app/count-to-lunar-new-year-eve/countdown.service.ts
+++ src/app/count-to-lunar-new-year-eve/countdown.service.ts
@@ -1,38 +1,41 @@
 import { Injectable } from '@angular/core';
-import {CalendarVietnamese} from 'date-chinese';
+import { CalendarVietnamese } from 'date-chinese';
 // const CalendarVietnamese = require('date-chinese').CalendarVietnamese;
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountdownService {
-  nextNewYearEve: any[5] ;
+  nextNewYearEve: any[5];
   // convertedToGregorian: any;
   today: Date;
-  convertedToGregorian : Date;
+  convertedToGregorian: Date;
 
   constructor() {
     this.today = new Date();
-    let nextYear = this.today.getFullYear()+1;  //2020
+    let nextYear = this.today.getFullYear() + 1;  //2020
     let lunarCal = new CalendarVietnamese();
-    
+
     let nextYearConverterToGregorian = lunarCal.newYear(nextYear);
     lunarCal.fromJDE(nextYearConverterToGregorian);
     let gregorianDate = lunarCal.toGregorian();
 
-    let previousNextYearConverterToGregorian = lunarCal.newYear(nextYear-1);// the year previous of "nextYear"
+    let previousNextYearConverterToGregorian = lunarCal.newYear(nextYear - 1);// the year previous of "nextYear"
     lunarCal.fromJDE(previousNextYearConverterToGregorian);
     let previousGregorianDate = lunarCal.toGregorian();//the previous "new Year eve" in Lunar
 
-    let gregorianDateGetTime = new Date(gregorianDate.year,gregorianDate.month-1, gregorianDate.day).getTime();
-    let previousGregorianDateGetTime = new Date(previousGregorianDate.year,previousGregorianDate.month-1, previousGregorianDate.day)
-                                                                                                                            .getTime();
+    let gregorianDateGetTime = new Date(gregorianDate.year, gregorianDate.month - 1, gregorianDate.day).getTime();
+    let previousGregorianDateGetTime = new Date(previousGregorianDate.year, previousGregorianDate.month - 1, previousGregorianDate.day)
+      .getTime();
 
-    if((gregorianDateGetTime - previousGregorianDateGetTime) > (gregorianDateGetTime-this.today.getTime()))
-      this.convertedToGregorian=  new Date(gregorianDate.year, gregorianDate.month-1, gregorianDate.day,0,0,0,0);
-                                                                                          //this is lunar new year date
-    else  this.convertedToGregorian=  new Date(previousGregorianDate.year, previousGregorianDate.month-1, previousGregorianDate.day);
-                                                        //this is also lunar new year date but after passing the Gregorian new Year Eve
+    if ((gregorianDateGetTime - previousGregorianDateGetTime) > (gregorianDateGetTime - this.today.getTime())) {
+      this.convertedToGregorian = new Date(gregorianDate.year, gregorianDate.month - 1, gregorianDate.day, 0, 0, 0, 0);
+      //this is lunar new year date
+    } else {
+      this.convertedToGregorian = new Date(previousGregorianDate.year, previousGregorianDate.month - 1, previousGregorianDate.day);
+      //this is also lunar new year date but after passing the Gregorian new Year Eve
+      // console.log('new year---' + this.convertedToGregorian.getFullYear());
+    }
 
     // console.log(gregorianDate);
   }
@@ -48,10 +51,11 @@ export class CountdownService {
   // getGregorianDay(lunar: any[]){
   // }
 
-  public countMiliSeconds(){
+  public countMiliSeconds() {
     return this.convertedToGregorian.getTime() - this.today.getTime();
   }
-  public countMonths(){
-    return this.convertedToGregorian.getMonth() - this.today.getMonth()+12;
+  public countMonths() {
+    return this.convertedToGregorian.getMonth() - this.today.getMonth() +
+      ((this.convertedToGregorian.getFullYear() < this.today.getFullYear()) ? 12 : 0);
   }
 }
--- src/app/today/today.component.ts
+++ src/app/today/today.component.ts
@@ -51,7 +51,7 @@ export class TodayComponent implements OnInit, DoCheck {
   ngDoCheck(): void {
   //   //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
   //   //Add 'implements DoCheck' to the class.
-    // document.querySelector(".root").style.backgroundImage = (this.link[0] || "");
+    // document.querySelector(".root").style.backgroundImage = (this.link[0] || ""); //wrong
   }
 
   //FUNCTIONs
